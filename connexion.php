3819b972d423012b73d242e55de08a34607b028f
sanji69 <axel.aubry69@gmail.com>
2018-04-04 09:07:31 +0200

    'connexion'

---
+++ connexion.php
@@ -0,0 +1,74 @@
+<?php
+include('inc/pdo.php');
+include('inc/function.php');
+
+$error = array();
+$connexion = false;
+
+if(!empty($_POST['send']))
+{
+  $login = trim(strip_tags($_POST['pseudo']));
+  $password = trim(strip_tags($_POST['password']));
+
+
+  if(!empty($login) && !empty($password))
+  {
+    $cryptopass = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "SELECT * FROM `users` WHERE pseudo= :pseudo";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+    $query-> execute();
+    $user = $query->fetch();
+    if(!empty($user))
+    {
+      $connectionstatus = password_verify($password, $user['password']);
+      if($user['pseudo'] != $login)
+        $error['pseudo'] = 'identifant incorrect';
+      elseif($connectionstatus == false)
+        $error['password'] = 'mot de passe incorrect';
+    }
+    $error['pseudo'] = 'identifiant incorrect';
+  }
+  $error['pseudo'] = 'identifiant incorrect';
+  if(count($error) == 0)
+    $connexion = true;
+  if($connexion == true)
+  {
+
+    $user_ip = getUserIP();
+    $_SESSION['user'] = array(
+      'login' = $login,
+      'user_ip' = $user_ip
+    );
+    header('location: index.php');
+  }
+}
+
+ ?>
+
+
+
+ <?php
+ include('inc/header.php');
+ ?>
+
+
+<form action="connexion.php" method="post">
+
+  <label>Pseudo</label>
+  <input type="text" name="pseudo" value="">
+  <span></span>
+
+  <label>Password</label>
+  <input type="text" name="password" value="">
+  <span></span>
+
+  <input type="submit" name="send" value="Connexion">
+
+</form>
+
+
+
+ <?php
+include('inc/footer.php');
+  ?>
---
+++ inc/function.php
@@ -0,0 +1,53 @@
+<?php
+
+function is_login_ok($login)
+{
+  if(!empty($login))
+  {
+    if(strlen($login)<5)
+      return 'Identifient trop court';
+    elseif(strlen($login)>50)
+      return 'Identifient trop court';
+  }
+  else
+    return 'Veuillez entrer un Identifient';
+}
+
+
+function is_mail_ok($mail)
+{
+  if(!empty($mail))
+  {
+    if(strlen($mail)<5)
+      return 'Email trop court';
+    elseif(strlen($mail)>100)
+      return 'Email trop long';
+    elseif(!filter_var($value, FILTER_VALIDATE_EMAIL))
+      return 'Veuillez renseigner une adresse email valide';
+    }
+    else
+        return 'Veuillez entrer un Email';
+}
+
+function is_password_ok($password, $pass)
+{
+  if(!empty($password))
+  {
+    if (strlen($password<5))
+      return 'Password trop court';
+    elseif (strlen($password>100))
+      return 'Password trop long';
+    elseif($password != $pass)
+      return 'Password doit êtes identique';
+  }
+  else
+    return 'Veuillez saisir un password';
+}
+
+function creatToken()
+{
+  $char = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $mot_de_passe = str_shuffle($char);
+  $mot_de_passe = substr($mot_de_passe ,0, 50);
+  return $mot_de_passe;
+}
