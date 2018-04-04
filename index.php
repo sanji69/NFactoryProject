22bdfacb15aaa8352b05058895f098c433856678
Bryan Randriam <bryan.randriam@gmail.com>
2018-04-04 10:21:29 +0200

    'index'

--- index.php
+++ index.php
@@ -1,19 +1,72 @@
-<?php
+<?php 
 include('inc/pdo.php');
+include('inc/function.php');
+?>
 
- ?>
+<?php
+include('inc/header.php');
+?>
+<div class="container-fluid">
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-40" src="images/13286.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-40" src="images/13288.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-40" src="images/13296.jpg" alt="Third slide">
+    </div>
+  </div>
+</div>
+</div>
+<div class="container-fluid">
+ <div class="container-fluide">
+</div>
+   <!-- Content here -->
+  <img src="images/4238.jpg">
+  <img src="images/4239.jpg">
+  <img src="images/4240.jpg">
+  <img src="images/4350.jpg">
+  <img src="images/4452.jpg">
+  <img src="images/4243.jpg">
+  <img src="images/4244.jpg">
+  <img src="images/4245.jpg">
+  <img src="images/13270.jpg">
+  <img src="images/4247.jpg">
+  <img src="images/4248.jpg">
+  <img src="images/4249.jpg">
+  <img src="images/4250.jpg">
+  <img src="images/4251.jpg">
+  <img src="images/4252.jpg">
+  <img src="images/4254.jpg">
+  <img src="images/4255.jpg">
+  <img src="images/4256.jpg">
+  <img src="images/4257.jpg">
+  <img src="images/4258.jpg">
+  <img src="images/13286.jpg">
+  <img src="images/4260.jpg">
+  <img src="images/4261.jpg">
+  <img src="images/4262.jpg">
+  <img src="images/4263.jpg">
+  <img src="images/4264.jpg">
+  <img src="images/13274.jpg">
+  <img src="images/4267.jpg">
+  <img src="images/4268.jpg">
+</div>
+  <div class="container">
+  
+</img>
+</div>
+<div class="footer">
+<button class="btn btn-danger"type="button" class="submit"> + de Films! </button>
+  </div>
+
+  <script language="JavaScript">Images_Aleatoire();</script>
 
 
-
- <?php
- include('inc/header.php');
- ?>
-
-
-
-
-
-
- <?php
-include('inc/footer.php');
+  <?php 
+  include('inc/footer.php');
+  
   ?>
