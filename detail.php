fd225a1a8dcff69909d48d1a98869f4df9a5a733
sanji69 <axel.aubry69@gmail.com>
2018-04-04 10:28:13 +0200

    'detail'

--- detail.php
+++ detail.php
@@ -1,15 +1,37 @@
 <?php
 include('inc/pdo.php');
+include('inc/function.php');
 
+if(!empty($_GET['id']) && is_numeric($_GET['id']))
+{
+  $sql= "SELECT * FROM movies_full WHERE id= :id";
+  $query= $pdo->prepare($sql);
+  $query->bindValue(':id', $_GET['id'],PDO::PARAM_INT);
+  $query->execute();
+  $movie=$query->fetch();
+}
+else
+  header('location: index.php');
  ?>
 
 
 
  <?php
  include('inc/header.php');
+
  ?>
 
-
+<div class="affichage">
+  <ul>
+    <?php foreach ($movie as $value)
+    {
+      if ($value == $_GET['id'])
+        echo '<img src="posters/' .$value. '.jpg" alt="poster de ' .$movie['title']. '"/>';
+      else
+        echo '<li>' .$value. '</li>';
+    } ?>
+  </ul>
+</div>
 
 
 
